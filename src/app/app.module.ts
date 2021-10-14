2e3fc45516ff4b0be08c90bf05c033d7cb8b5f5d
duanwesh <nishant99.panigrahi@gmail.com>
2021-10-14 20:18:23 +0530

    Master push

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IconComponent } from './components/icon/icon.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
+
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+@NgModule({
+  declarations: [
+    AppComponent,
+    IconComponent,
+      ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
